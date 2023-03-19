232036dfa5c6eb3d45d278fefcb44f6bdcb4d149
krupalpatel45 <krupalpatel1998@gmail.com>
2023-03-18 18:29:39 -0600

    Updated code
    - Changed logo
    - Changed default map
    - User page
    - Error page

---
+++ AppCode/UniGo_Code_V3.3/frontend/next.conf.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+module.exports = {
+    env: {
+        REACT_APP_MAPS_API_KEY: "AIzaSyCCZcb_AEAcCRk0uxe-GjAtUU_ewjpDXIM",
+        REACT_APP_END_POINT: "http://18.224.165.108:8080/api",
+    }
+}
